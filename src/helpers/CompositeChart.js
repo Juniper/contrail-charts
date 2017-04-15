69b8a35115edf8be34c7c5b369ae24ed75302809
Dmitra <adrian@dmitra.com>
2017-04-15 15:55:40 +0300

    convert ChartView into CompositeChart helper
    this helper will be used in Views utilizing multiple components

--- src/helpers/CompositeChart.js
+++ src/helpers/CompositeChart.js
@@ -2,25 +2,23 @@
  * Copyright (c) Juniper Networks, Inc. All rights reserved.
  */
 import _ from 'lodash'
-import * as Components from 'components/index'
-import * as Providers from 'providers/index'
-import * as Actions from 'actions/index'
+import * as Components from 'components'
+import * as Composites from 'composites'
+import * as Providers from 'providers'
+import * as Actions from 'actions'
 import TitleView from 'helpers/title/TitleView'
 import actionman from 'core/Actionman'
 /**
+ * Creates a chart as a composition of components or other compositions
 */
-export default class ChartView {
+export default class CompositeChart {
   constructor (p) {
-    this._config = null
     this._components = []
-    this._container = null
-    this._provider = null
-
+    this.actionman = actionman
     this._frozen = false
+    this.setConfig(p)
   }
-  /**
-  * Provide data for this chart as a simple array of objects.
-  */
+
   setData (data) {
     if (this.frozen) return
     if (!_.isArray(data)) return
@@ -34,7 +32,7 @@ export default class ChartView {
    * Sets the configuration for this chart as a simple object.
    * Instantiate the required views if they do not exist yet, set their configurations otherwise.
    * Updating configuration to a rendered chart will trigger a ConfigModel change event that will cause the chart to be re-rendered.
-   * calling setConfig on already rendered chart will reset the chart.
+   * calling setConfig on already rendered chart will update the chart.
    */
   setConfig (config) {
     if (this._config) this.remove()
@@ -47,9 +45,10 @@ export default class ChartView {
      */
     _.each(Actions, action => actionman.set(action, this))
 
-    // create common provider for all components to prepare (format) data just once
+    // if config specified create common provider for all components to prepare (format) data just once
     if (config.provider && config.provider.type) {
-      this._provider = new [`${config.provider.type}Provider`](config.provider.config)
+      const Provider = Providers[config.provider.type + 'Provider']
+      if (Provider) this._provider = new Provider(config.provider.config)
     }
     this._initComponents()
   }
@@ -57,21 +56,47 @@ export default class ChartView {
    * Get component by id
    * @param {String} id
    */
-  getComponent (id) {
+  get (id) {
     return _.find(this._components, {id: id})
   }
   /**
-   * Get array of components by type
-   * @return {Array}
+   * @param {String} type
+   * @return {Array} of components
    */
-  getComponentsByType (type) {
+  getByType (type) {
     return _.filter(this._components, {type: type})
+  }
+  /**
+   * Initialize individual component
+   * @param {String} type
+   * @param {Object} config
+   * @param {Object} providerConfig
+   * @param {Provider} model optional for dependent components
+   */
+  add ({id, type, config, provider: providerConfig}, model) {
+    if (!_.isObject(config) || config.enable === false) return false
+    const Component = Components[type + 'View'] || Composites[type + 'View']
+
+    //const container = this._container.querySelector(`[component="${config.container || config.id}"]`)
+    model = model || this._provider
+
+    // Share first initialized provider with all other components
+    //if (!this._provider) this._provider = model
+
+    const viewOptions = {
+      id: id || config.id,
+      config,
+      model,
+      container: config.container || this._container,
+    }
+    const component = new Component(viewOptions)
+    this._components.push(component)
+
+    return component
   }
 
   render () {
-    _.each(this._components, (component) => {
-      component.render()
-    })
+    _.each(this._components, component => component.render())
   }
   /**
    * Removes chart view and its components.
@@ -82,26 +107,12 @@ export default class ChartView {
     _.each(this._components, component => component.remove())
     this._components = []
   }
-
-  renderMessage (msgObj) {
-    actionman.fire('SendMessage', msgObj)
-  }
-
-  clearMessage (componentId) {
-    // To clear messages for a given component we send a message with 'update' action and an empty array of messages.
-    const msgObj = {
-      componentId: componentId,
-      action: 'update',
-      messages: [],
-    }
-    actionman.fire('ClearMessage', msgObj)
-  }
   /**
    * Initialize configured components
    */
   _initComponents () {
     // Apply template
-    this._container = document.querySelector('#' + this._config.id)
+    this._container = this._config.container || document.querySelector('#' + this._config.id)
     if (this._config.template) {
       const template = document.createElement('template')
       template.innerHTML = this._config.template()
@@ -118,57 +129,11 @@ export default class ChartView {
       component.config.id = component.id
     })
     const [dependent, independent] = _.partition(this._config.components, c => c.config.sourceComponent)
-    _.each(independent, component => this._registerComponent(component))
+    _.each(independent, component => this.add(component))
     _.each(dependent, component => {
       const sourceComponent = this.getComponent(component.config.sourceComponent)
-      const componentView = this._registerComponent(component, sourceComponent.model)
+      const componentView = this.add(component, sourceComponent.model)
       componentView.config.parent = sourceComponent.config
     })
-  }
-  /**
-   * Initialize individual component
-   * @param {String} type
-   * @param {Object} config
-   * @param {Object} providerConfig
-   * @param {Provider} model optional for dependent components
-   */
-  _registerComponent ({type, config, provider: providerConfig}, model) {
-    if (!this._isEnabledComponent(type)) return false
-    const Component = Components[type + 'View']
-    const ConfigModel = Components[type + 'ConfigModel']
-    const Provider = Providers[Component.dataType + 'Provider']
-
-    let configModel
-    if (ConfigModel) configModel = new ConfigModel(config)
-    const container = this._container.querySelector(`[component="${config.container || config.id}"]`)
-    model = model || this._provider
-    if (Provider && (!model || providerConfig)) model = new Provider(null, providerConfig)
-
-    // Share first initialized provider with all other components
-    if (!this._provider) this._provider = model
-    const viewOptions = {
-      id: config.id,
-      config: configModel,
-      model: model,
-      container: container || this._container,
-    }
-    const component = new Component(viewOptions)
-    this._components.push(component)
-
-    return component
-  }
-
-  _isEnabled (config, type) {
-    const foundConfig = _.find(config, {type: type})
-    if (!foundConfig) return false
-    if (_.isObject(foundConfig.config)) {
-      return !(foundConfig.config.enable === false)
-    }
-    return false
-  }
-
-  // TODO this is not enough specific
-  _isEnabledComponent (type) {
-    return this._isEnabled(this._config.components, type)
   }
 }
