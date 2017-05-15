17796a5e5be066cb4937c5008ae93fce41ba9665
Dmitra <adrian@dmitra.com>
2017-05-15 08:29:13 +0300

    factor out common data model code
    create unspecific general Data model to be used with components which can work with any data format

--- src/models/Serie.js
+++ src/models/Serie.js
@@ -2,36 +2,10 @@
  * Copyright (c) Juniper Networks, Inc. All rights reserved.
  */
 import _ from 'lodash'
-import Events from 'contrail-events'
+import DataModel from './Data'
 
-export default class SerieProvider {
-  constructor (data, config) {
-    this.data = data
-    this.config = config
-  }
-
-  get data () {
-    return this._data
-  }
-
-  set data (data) {
-    this._data = this.parse(data) || []
-    this.trigger('change')
-  }
-
-  set config ({formatter} = {}) {
-    if (!formatter) return
-    this._formatter = formatter
-    this.trigger('change')
-  }
-
-  parse (data) {
-    return _.isFunction(this._formatter) ? this._formatter(data) : data
-  }
-
-  getLabels (formatter) {
-    return _.map(this._data, serie => formatter(serie))
+export default class SerieModel extends DataModel {
+  get accessors () {
+    return _.keys(this._data)
   }
 }
-// TODO replace with class extends syntax
-_.extend(SerieProvider.prototype, Events)
