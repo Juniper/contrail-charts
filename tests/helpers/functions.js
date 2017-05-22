03a8634e5b3df5721be12d597f8f4f3670a38b1d
Sergey <slivers@ukr.net>
2017-05-22 17:33:12 +0300

    Add pie tests

--- tests/helpers/functions.js
+++ tests/helpers/functions.js
@@ -1,3 +1,10 @@
+/**
+ * This function run callback after DOM event on target element
+ * @param type - attribute or element
+ * @param el - target element
+ * @param attrName - target attribute
+ * @param callback
+ */
 function observer (type, el, attrName, callback) {
   let config
   if (type === 'attr') {
@@ -15,4 +22,16 @@ function observer (type, el, attrName, callback) {
     })
   })
   observer.observe(el, config)
+}
+
+/**
+ * convert a hexidecimal color string to rgb string
+ * @param hex
+ * @returns {string}
+ */
+function hexToRGB (hex) {
+  let r = hex >> 16
+  let g = hex >> 8 & 0xFF
+  let b = hex & 0xFF
+  return `rgb(${r}, ${g}, ${b})`
 }
--- tests/specs/components/Pie.js
+++ tests/specs/components/Pie.js
@@ -40,6 +40,25 @@ describe('PieView', () => {
       chart.setData(data)
       expect(container.querySelectorAll('path.arc').length).toEqual(4)
     })
+
+    it('should render with default color schema', () => {
+      config = {
+        serie: {
+          getValue: v => v.y,
+        }
+      }
+      chart.setConfig(config)
+      chart.setData(data)
+      let chartSectors = container.querySelectorAll('path.arc')
+      let i = 0
+      _.forEach(chartSectors, function (sector) {
+        let hex = d3.schemeCategory20[i]
+        let rgb = hexToRGB(parseInt(hex.slice(1), 16))
+        let color = sector.style.fill
+        expect(color).toBe(rgb)
+        i++
+      })
+    })
   })
 
   describe('Render with changed config.', () => {
@@ -50,6 +69,114 @@ describe('PieView', () => {
       let path = container.querySelector('path.arc')
       let d = path.getAttribute('d')
       expect(d).toContain('L0,0Z')
+    })
+
+    it('should be change radius', () => {
+      config.radius = 150
+      chart.setConfig(config)
+      chart.setData(data)
+      let path = container.querySelector('path.arc')
+      let d = path.getAttribute('d')
+      expect(d).toContain('A150,150')
+    })
+
+    it('should be change radius and type', () => {
+      config.radius = 150
+      config.type = 'pie'
+      chart.setConfig(config)
+      chart.setData(data)
+      let path = container.querySelector('path.arc')
+      let d = path.getAttribute('d')
+      expect(d).toContain('A150,150')
+      expect(d).toContain('L0,0Z')
+    })
+  })
+
+  describe('Render with data variants', () => {
+    it('should render empty chart without data', () => {
+      chart.render()
+      expect(container.querySelector('path.arc')).toBeNull()
+    })
+
+    it('should render empty chart with empty data', () => {
+      chart.setData([])
+      expect(container.querySelector('path.arc')).toBeNull()
+    })
+
+    it('should render chart with one sector', () => {
+      chart.setData([{ x: 1, y: 2 }])
+      expect(container.querySelectorAll('path.arc').length).toBe(1)
+    })
+
+    it('should render chart with two sectors', () => {
+      chart.setData([
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+      ])
+      expect(container.querySelectorAll('path.arc').length).toEqual(2)
+    })
+
+    it('should render chart with two sectors with some color', () => {
+      chart.setData([
+        { x: 1, y: 2 },
+        { x: 1, y: 3 }
+      ])
+      let firstElement = container.querySelectorAll('path.arc')[0]
+      let secondElement = container.querySelectorAll('path.arc')[1]
+      expect(firstElement.style.fill).toBe(secondElement.style.fill)
+    })
+
+    it('should render chart with two some sectors', () => {
+      chart.setData([
+        { x: 1, y: 2 },
+        { x: 1, y: 2 }
+      ])
+      let firstD = container.querySelectorAll('path.arc')[0].getAttribute('d')
+      let secondD = container.querySelectorAll('path.arc')[1].getAttribute('d')
+      let firstElementStartPosition = firstD.slice(0, firstD.indexOf('A')).split(',')
+      let secondElementStartPosition = secondD.slice(0, secondD.indexOf('A')).split(',')
+
+      expect(firstElementStartPosition[0]).toBe(secondElementStartPosition[0])
+      expect(Math.abs(+firstElementStartPosition[1])).toBe(+secondElementStartPosition[1])
+    })
+
+    it('should render chart whith NaN data on y', () => {
+      data = [
+        {x: '1', y: 1},
+        {x: '2', y: NaN},
+        {x: '3', y: 3}
+      ]
+      chart.setData(data)
+      let sector = container.querySelectorAll('path.arc')[1]
+      let d = sector.getAttribute('d')
+
+      expect(d).not.toContain('A')
+    })
+
+    it('should render chart whith undefined data on y', () => {
+      data = [
+        {x: '1', y: 1},
+        {x: '2', y: undefined},
+        {x: '3', y: 3}
+      ]
+      chart.setData(data)
+      let sector = container.querySelectorAll('path.arc')[1]
+      let d = sector.getAttribute('d')
+
+      expect(d).not.toContain('A')
+    })
+
+    it('should render chart whith null data on y', () => {
+      data = [
+        {x: '1', y: 1},
+        {x: '2', y: null},
+        {x: '3', y: 3}
+      ]
+      chart.setData(data)
+      let sector = container.querySelectorAll('path.arc')[1]
+      let d = sector.getAttribute('d')
+
+      expect(d).not.toContain('A')
     })
   })
 })
