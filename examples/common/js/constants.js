6a7b1203f1cde6d7e7edc4984b24d26ca1245511
Dovlash <dunduvlash@gmail.com>
2017-06-11 15:38:15 +0200

    - Removed commented out colors in constants file

--- examples/common/js/constants.js
+++ examples/common/js/constants.js
@@ -4,65 +4,6 @@
 
 import {schemeCategory10 as d3ColorScheme10, schemeCategory20 as d3ColorScheme20, schemeCategory20c as d3ColorScheme20c} from 'd3-scale'
 
-/*
-const lbColorScheme7 = [
-  '#1f77b4',
-  '#aec7e8',
-  '#ff7f0e',
-  '#ffbb78',
-  '#2ca02c',
-  '#98df8a',
-  '#d62728'
-]
-
-const lbColorScheme17 = [
-  '#1f77b4',
-  '#aec7e8',
-  '#ff7f0e',
-  '#ffbb78',
-  '#2ca02c',
-  '#98df8a',
-  '#d62728',
-  '#ff9896'
-].concat(d3ColorScheme10)
-
-const bubbleColorScheme6 = [
-  '#3f51b5',
-  d3ColorScheme10[1],
-  d3ColorScheme10[0],
-  d3ColorScheme10[2],
-  '#424242',
-  '#9c27b0'
-]
-
-const bubbleColorScheme13 = [
-  '#424242',
-  '#9c27b0',
-  '#3f51b5'
-].concat(d3ColorScheme10)
-
-const radialColorScheme6 = [
-  '#00bcd4',
-  '#4caf50',
-  '#a88add',
-  '#fcc100',
-  '#2196f3',
-  '#c62828'
-]  
-
-const radialColorScheme10 = [
-  lbColorScheme7[0],
-  lbColorScheme7[1],
-  lbColorScheme7[4],
-  lbColorScheme7[2],
-  lbColorScheme7[3],
-  lbColorScheme7[5],
-  lbColorScheme7[6],
-  '#ff9896',
-  '#9467bd',
-  '#c5b0d5'
-] */
-
 const palette = d3ColorScheme20
 
 const paletteSoft = [
